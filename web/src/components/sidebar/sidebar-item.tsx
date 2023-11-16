df28d1efb93565bf50826b9b0693117f0fe49507
Ben <benhoeg@outlook.com>
2023-11-15 23:21:41 -0400

    chore: add active state to sidebar item

--- web/src/components/sidebar/sidebar-item.tsx
+++ web/src/components/sidebar/sidebar-item.tsx
@@ -1,19 +1,26 @@
 'use client'
 
+import { cn } from '@/utils/cn'
 import Link, { type LinkProps } from 'next/link'
 import type { FC, ReactNode } from 'react'
+import { usePathname } from 'next/navigation'
 
 type Props = {
   children: ReactNode
 } & LinkProps
 
 const SidebarItem: FC<Props> = ({ children, href, ...rest }) => {
+  const pathname = usePathname()
+
   return (
     <Link
       href={href}
       draggable={false}
       {...rest}
-      className="flex h-10 select-none items-center rounded-md px-3 font-medium text-stone-400 "
+      className={cn(
+        'flex h-10 select-none items-center rounded-md px-3 font-medium text-[#7B8188]',
+        pathname === href && 'text-[#fff]',
+      )}
     >
       {children}
     </Link>
