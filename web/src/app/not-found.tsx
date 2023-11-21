68d7386f39e215311add0e1255ec57c81e5a3b44
Ben <benhoeg@outlook.com>
2023-11-21 02:51:59 -0400

    feat(web): added basic 404 page

---
+++ web/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <div className="flex w-full flex-col items-center justify-center">
+      <h1 className="mb-4 text-2xl font-bold leading-tight md:text-8xl">404</h1>
+      <p className="px-6 text-center text-xl text-[#888]">Page not found</p>
+    </div>
+  )
+}
+
+export default NotFoundPage
