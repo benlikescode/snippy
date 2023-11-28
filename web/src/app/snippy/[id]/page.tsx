3f36999a39c0fbad90df5dbec6d44357f2dc7911
Ben <benhoeg@outlook.com>
2023-11-28 15:07:42 -0400

    redirect to login if no auth on snippy pages

--- web/src/app/snippy/[id]/page.tsx
+++ web/src/app/snippy/[id]/page.tsx
@@ -1,6 +1,6 @@
 import Snippy from '@/app/snippy/_components/snippy'
 import { type FC } from 'react'
-import { notFound } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
 import { db } from '@/server/db'
 import { getServerAuthSession } from '@/server/auth'
 
@@ -13,7 +13,7 @@ const SnippyPage: FC<Props> = async ({ params: { id } }) => {
 
   const session = await getServerAuthSession()
 
-  if (!session?.user) return notFound()
+  if (!session?.user) return redirect('/login')
 
   const snippy = await db.template.findFirst({
     where: {
--- web/src/app/snippy/new/page.tsx
+++ web/src/app/snippy/new/page.tsx
@@ -1,6 +1,12 @@
 import Snippy from '@/app/snippy/_components/snippy'
+import { getServerAuthSession } from '@/server/auth'
+import { redirect } from 'next/navigation'
 
-const NewSnippyPage = () => {
+const NewSnippyPage = async () => {
+  const session = await getServerAuthSession()
+
+  if (!session?.user) return redirect('/login')
+
   return <Snippy />
 }
 
