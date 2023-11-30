ba7b81bc0f4ec1d91c148c9c195e3198a3300a6b
Ben <benhoeg@outlook.com>
2023-11-29 20:11:39 -0400

    add customized greeting to home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -7,11 +7,25 @@ const HomePage = async () => {
 
   if (!session?.user) return redirect('/login')
 
+  const getGreeting = () => {
+    const currentHour = new Date().getHours()
+    const greeting =
+      currentHour < 12 ? 'Good morning' : currentHour < 18 ? 'Good afternoon' : 'Good evening'
+
+    if (!session.user.name) {
+      return greeting
+    }
+
+    const name = session.user.name.split(' ')[0]
+
+    return `${greeting} ${name}`
+  }
+
   return (
     <main id="main" className="w-full overflow-y-auto">
       <div className="mx-auto my-16 max-w-screen-lg px-4">
         <div>
-          <h2 className="mb-2 text-2xl font-bold">Welcome Back</h2>
+          <h2 className="mb-2 text-2xl font-bold">{getGreeting()}</h2>
           <p className="text-[#868686]">Speed up component creation & usage.</p>
         </div>
 
