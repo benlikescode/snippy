150eb37c64b48356c242d6acd0a765f16ddc0bf7
Ben <benhoeg@outlook.com>
2023-12-12 21:46:45 -0400

    chore: create community page placeholder

---
+++ web/src/app/community/page.tsx
@@ -0,0 +1,9 @@
+const CommunityPage = () => {
+  return (
+    <h1 className="flex h-full w-full items-center justify-center text-2xl font-bold">
+      Coming Soon!
+    </h1>
+  )
+}
+
+export default CommunityPage
