da37fc85d15a79a2caf84f1a24241032e707e5ab
Ben <benhoeg@outlook.com>
2024-01-09 15:57:20 -0400

    chore: show improved error message

--- extension/src/utils/auth.ts
+++ extension/src/utils/auth.ts
@@ -10,5 +10,9 @@ export const handleAuthPrompt = async () => {
 export const getAccessToken = async () => {
   const session = await vscode.authentication.getSession(GITHUB_AUTH_PROVIDER_ID, SCOPES)
 
-  return session?.accessToken || ''
+  if (!session?.accessToken) {
+    throw new Error('Please login with GitHub to use the Snippy extension')
+  }
+
+  return session.accessToken
 }
